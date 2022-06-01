12c25d7c0dd29ae8bafacb022f6023225fcc2ef6
stephaniepboyne <stephanie_boyne@yahoo.co.uk>
2022-06-01 09:47:50 +0100

    Added annotation

--- bookfestival/src/main/java/com/CodeClan/example/bookfestival/controllers/EventController.java
+++ bookfestival/src/main/java/com/CodeClan/example/bookfestival/controllers/EventController.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class EventController {
 
