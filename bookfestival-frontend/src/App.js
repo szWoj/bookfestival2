39863847e3b2964750014dc9077f445e3c262404
stephaniepboyne <stephanie_boyne@yahoo.co.uk>
2022-06-01 10:56:49 +0100

    empty calendar built

--- bookfestival-frontend/src/App.js
+++ bookfestival-frontend/src/App.js
@@ -3,7 +3,9 @@ import React from 'react';
 import VenueComponent from './components/Venue';
 import Home from './components/WhatsOn';
 import SingleEvent from './components/SingleEvent';
+import Calendar from './components/Calendar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import EventCalendar from './components/Calendar';
 
 
 
@@ -23,6 +25,7 @@ function App(){
              <Route exact path="/" element={<Home/>}/>
              <Route exact path="/venues" element={<VenueComponent/>}/>
              <Route exact path="/event/:id" element={<SingleEvent/>}/>
+             <Route exact path="/calendar" element={<EventCalendar/>}/>
 
            </Routes>
         </div> 
