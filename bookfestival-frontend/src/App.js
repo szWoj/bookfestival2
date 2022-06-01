acc99ef18a33635963ead0ee131369a217b015fd
stephaniepboyne <stephanie_boyne@yahoo.co.uk>
2022-06-01 12:00:52 +0100

    booking page started

--- bookfestival-frontend/src/App.js
+++ bookfestival-frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import VenueComponent from './components/Venue';
 import Home from './components/WhatsOn';
 import SingleEvent from './components/SingleEvent';
+import BookEvent from './components/BookEvent';
 import Calendar from './components/Calendar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import EventCalendar from './components/Calendar';
@@ -26,6 +27,7 @@ function App(){
              <Route exact path="/venues" element={<VenueComponent/>}/>
              <Route exact path="/event/:id" element={<SingleEvent/>}/>
              <Route exact path="/calendar" element={<EventCalendar/>}/>
+             <Route exact path="/book-event/:id" element={<BookEvent/>}/>
 
            </Routes>
         </div> 
--- bookfestival-frontend/src/components/BookEvent.js
+++ bookfestival-frontend/src/components/BookEvent.js
@@ -1,8 +1,35 @@
 import { Link } from "react-router-dom";
+import { useParams } from 'react-router';
+import { useState, useEffect} from "react";
+import EventService from "../services/EventService";
 
 const BookEvent = () => {
+
+    const{id} = useParams();
+    const[eventId, setEventId] = useState([id])
+    const [event, setEvent] = useState([])
+
+    useEffect(() => {
+        getEvents()
+    }, [])
+
+    const getEvents = () => {
+
+        EventService.getEvents().then((response) => {
+            setEvent(response.data[eventId])
+            console.log(response.data[eventId]);
+        });
+    };
+
+
     return (
-        <h1>Book an Event</h1>
+        <>
+        <h1>Book Event</h1>
+        <p>{event.title}</p>
+        <p>Date and time: {event.dateTime}</p>
+        <p>Price: £{event.price}</p>
+        <button>Book Now</button>
+        </>
     )
 }
 
