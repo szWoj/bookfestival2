13b35a181960edf498b5d56ecf78e937f5bd916c
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-06-03 15:22:54 +0100

    all changes for single event

--- bookfestival-frontend/src/components/SingleEvent.js
+++ bookfestival-frontend/src/components/SingleEvent.js
@@ -11,7 +11,7 @@ const SingleEvent = () => {
     const[eventId, setEventId] = useState([id])
     const [event, setEvent] = useState([])
     const [venues, setVenues] = useState(["Dummy Venue Name"])
-    // const [venueId, setVenueId] = useState([])
+    const [venueId, setVenueId] = useState([])
 
     useEffect(() => {
         getEvents()
@@ -26,8 +26,18 @@ const SingleEvent = () => {
         EventService.getEvents().then((response) => {
             setEvent(response.data[eventId])
             console.log(response.data[eventId])
+            //get the venue id based on the event id
+            // const tempVenueId = getVenueId(eventId) 
         });
     };
+
+    // this function should get the venue id based on the event id
+    // then can feed it into the html instead of 0 in venues[0].name
+    // const getVenueId = (eventId) => {
+    //     VenueService.getVenues().then((response) => {
+    //         setVenueId
+    //     })
+    // }
 
     const getVenueName = () => {
         VenueService.getVenues().then((response) => {
