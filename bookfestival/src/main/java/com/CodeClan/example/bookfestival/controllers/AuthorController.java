0b58a3ac00ebd0739965b2c8531a018af7e8be3f
thumminbird14 <tanyapurcell@hotmail.co.uk>
2022-06-07 16:11:53 +0100

    management information

--- bookfestival/src/main/java/com/CodeClan/example/bookfestival/controllers/AuthorController.java
+++ bookfestival/src/main/java/com/CodeClan/example/bookfestival/controllers/AuthorController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-@Controller
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
 public class AuthorController {
 
     @Autowired
