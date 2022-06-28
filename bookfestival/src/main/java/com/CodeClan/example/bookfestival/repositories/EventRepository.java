5c94c58a81a36c6b2a51052f51c3962c620f6495
stephaniepboyne <stephanie_boyne@yahoo.co.uk>
2022-06-28 10:28:20 +0100

    eventcontroller fixed

--- bookfestival/src/main/java/com/CodeClan/example/bookfestival/repositories/EventRepository.java
+++ bookfestival/src/main/java/com/CodeClan/example/bookfestival/repositories/EventRepository.java
@@ -5,6 +5,13 @@ import com.CodeClan.example.bookfestival.models.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByPriceEquals(int number);
+
+    List<Event> findByPriceGreaterThanEqual(int number);
+
+    List<Event> findByPriceBetween(int number1, int number2);
 }
